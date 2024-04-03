579fa779db3a1d92577777a2ccb53c7ecd997aed
Alex <velengura.alex@gmail.com>
2024-04-03 18:47:14 +0300

    Added VillaApies Dtos, added APIRequest with project ref from MagicVilla_Utility

---
+++ Models/APIRequest.cs
@@ -0,0 +1,13 @@
+﻿using static MagicVilla_Utility.StaticDetails;
+
+namespace MagicVilla_Web.Models
+{
+    public class APIRequest
+    {
+        // RCl => Add => add prject ref => MagicVilla_Utility
+        public ApiType ApiType { get; set; } = ApiType.GET;
+
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
